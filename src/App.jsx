2a48661dc4c5867cc083ccf65f869d2614ff928f
ornab <ornab@ornab.ca>
2024-06-19 16:05:47 -0400

    Initial app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import "./App.css";
+import SkinToneSelector from "./components/SkinToneSelector";
 
 function App() {
-  return <h1 className="text-3xl font-bold">Hello world!</h1>;
+  return (
+    <div>
+      <main>
+        <SkinToneSelector />
+      </main>
+    </div>
+  );
 }
 
 export default App;
