976941c1195c9d09408083f166ff8285d94be6c7
ornab <ornab@ornab.ca>
2024-06-20 21:10:43 -0400

    Add styling

--- src/components/SkinToneSelector.jsx
+++ src/components/SkinToneSelector.jsx
@@ -95,16 +95,36 @@ const SkinToneSelector = () => {
   };
 
   return (
-    <div className="flex flex-col justify-center items-center min-h-screen">
+    <div className="flex flex-col justify-center items-center min-h-screen bg-slate-950">
       <div className="w-full max-w-4xl p-4 md:p-8">
         {!imageUploaded && (
-          <div className="text-center">
-            <p className="text-gray-600 text-lg mb-4">Please upload an image</p>
+          <div className="text-center p-8 bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 rounded-lg shadow-lg">
+            <p className="text-white text-xl mb-6 font-semibold">
+              Upload an image to get started
+            </p>
             <label
               htmlFor="fileInput"
-              className="bg-slate-500 hover:bg-slate-700 text-white font-bold py-2 px-4 rounded cursor-pointer"
+              className="group relative inline-flex items-center justify-center p-0.5 mb-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 cursor-pointer"
             >
-              Upload Image
+              <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                <span className="flex items-center">
+                  <svg
+                    className="w-6 h-6 mr-2"
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"
+                    ></path>
+                  </svg>
+                  Choose Image
+                </span>
+              </span>
             </label>
             <input
               id="fileInput"
@@ -114,19 +134,42 @@ const SkinToneSelector = () => {
               onChange={handleImageUpload}
               className="hidden"
             />
+            <p className="text-white text-sm mt-4">
+              Supported formats: JPG, PNG, GIF
+            </p>
           </div>
         )}
         {imageUploaded && (
-          <div className="flex flex-col items-center">
+          <div className="flex flex-col items-center max-h-svh">
             <canvas
               ref={canvasRef}
               onClick={handleCanvasClick}
-              className="border border-gray-300 rounded-md mb-4"
+              className="rounded-xl mb-4"
             ></canvas>
-            <div className="text-center">
-              <p className="text-gray-600">Selected Color: {selectedColor}</p>
-              <p className="text-gray-600 text-2xl">Suggested Emoji:</p>
-              <p className="text-gray-600 text-4xl">{emoji}</p>
+            <div className="text-center bg-gray-300 p-6 rounded-lg shadow-md w-80 h-48 flex flex-col justify-center">
+              {emoji === "" ? (
+                <p className="text-gray-600 text-2xl">
+                  Click on the picture to sample a skin tone.
+                </p>
+              ) : (
+                <>
+                  <p className="text-gray-600 mb-2">
+                    Selected Color: {selectedColor}
+                  </p>
+                  <p className="text-gray-600 text-2xl mb-2">
+                    Suggested Emoji:
+                  </p>
+                  <p
+                    className={`text-gray-600 ${
+                      emoji === "No skin tone detected"
+                        ? "text-2xl"
+                        : "text-3xl"
+                    }`}
+                  >
+                    {emoji}
+                  </p>
+                </>
+              )}
             </div>
           </div>
         )}
