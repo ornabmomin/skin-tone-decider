3b4756a538d8a6bf894b6e7ed53bab739079f32f
ornab <ornab@ornab.ca>
2024-06-24 15:34:17 -0400

    Add favicon and update site title
    Header logo sizing tweaks
    
    Clean up names and paths to assets

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import PropTypes from "prop-types";
 import Modal from "./Modal";
-import logo from "../assets/sts-logo.png";
+import logo from "/sts-logo.png";
 
 const Header = ({ imageUploaded }) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -13,16 +13,20 @@ const Header = ({ imageUploaded }) => {
   return (
     <>
       <header className="fixed top-0 left-0 right-0 z-10">
-        <div className="container mx-auto px-4 py-2 flex justify-between items-center">
+        <div className="container mx-auto px-4 py-1 flex justify-between items-center">
           <div className="flex items-center">
             <img
               src={logo}
               alt="Logo"
               className={`${
                 imageUploaded ? "w-12 h-12" : "w-24 h-24"
-              } rounded-full mr-2`}
+              } rounded-full`}
             />
-            <span className="text-lg font-bold text-white">
+            <span
+              className={`${
+                imageUploaded ? "text-sm" : "text-lg"
+              } font-bold text-white`}
+            >
               Skin Tone Decider
             </span>
           </div>
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -4,14 +4,15 @@ const Modal = ({ toggleModal }) => {
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 backdrop-blur-sm flex items-center justify-center z-20 p-4">
       <div className="bg-white dark:bg-gray-800 rounded-xl shadow-2xl w-full max-w-md max-h-[80vh] overflow-hidden flex flex-col">
-        <div className="p-6 overflow-y-auto">
-          <h2 className="text-3xl font-bold mb-6 text-gray-800 dark:text-white">
-            About this app
+        <div className="p-8 overflow-y-auto">
+          <h2 className="text-2xl md:text-3xl font-bold mb-6 text-gray-800 dark:text-white">
+            About <p className="display: inline italic">Skin Tone Decider</p>
           </h2>
           <div className="text-gray-600 dark:text-gray-300">
             <p className="pb-4">
-              Skin Tone Selector is an educational tool that seeks to help
-              people decide which skin tone they should choose for their emojis.
+              <p className="display: inline italic">Skin Tone Decider</p> is an
+              educational tool that seeks to help people decide which skin tone
+              they should choose for their emojis.
             </p>
             <p className="pb-4">
               None of this has been fact checked by anyone or anything. I do not
@@ -24,13 +25,13 @@ const Modal = ({ toggleModal }) => {
             <p>Nah dawg that&apos;d be weird af ngl.</p>
           </div>
         </div>
-        <div className="px-6 py-4">
+        <div className="px-6 py-2 pb-8">
           <button
             onClick={toggleModal}
             className="w-full px-4 py-2 text-white rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2
        bg-gradient-to-r from-purple-600 via-pink-500 to-red-500
        hover:bg-gradient-to-l hover:from-purple-600 hover:via-pink-500 hover:to-red-500
-       transition-all duration-500 ease-in-out"
+       transition-all duration-500 ease-in-out hover:font-extrabold"
           >
             Close
           </button>
