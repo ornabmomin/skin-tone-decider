ab3d3f8b3fc2772399b2dc6b15bfaac25e76ade3
ornab <ornab@ornab.ca>
2024-07-05 12:03:39 -0400

    Refactor to use context

--- src/components/SkinToneSelector.jsx
+++ src/components/SkinToneSelector.jsx
@@ -1,187 +1,26 @@
-import { useState, useRef, useEffect, useCallback } from "react";
-import PropTypes from "prop-types";
+import { useContext, useRef } from "react";
 import UploadBox from "./UploadBox";
 import Canvas from "./Canvas";
 import ColourInfo from "./ColourInfo";
-import getSkinTone from "../utils/getSkinTone";
+import { SkinToneContext } from "../store/SkinToneContext";
 
-const SkinToneSelector = ({ setImageUploaded, imageUploaded }) => {
-  const [selectedColor, setSelectedColor] = useState("");
-  const [skinTone, setSkinTone] = useState({ emoji: null, tone: null });
-  const [lastClickPosition, setLastClickPosition] = useState(null);
-  const [sampleSize, setSampleSize] = useState(20);
-  const [maxSampleSize, setMaxSampleSize] = useState(50);
-  const [averageRGB, setAverageRGB] = useState(null);
+const SkinToneSelector = () => {
+  const { imageUploaded } = useContext(SkinToneContext);
 
-  const canvasRef = useRef(null);
-  const imageRef = useRef(null);
   const fileInputRef = useRef(null);
-
-  const handleImageUpload = (event) => {
-    const file = event.target.files[0];
-    const img = new Image();
-    img.onload = () => {
-      setImageUploaded(true);
-      imageRef.current = img;
-    };
-
-    if (file) {
-      const reader = new FileReader();
-      reader.onload = (event) => {
-        img.src = event.target.result;
-      };
-      reader.readAsDataURL(file);
-    }
-  };
-
-  const drawImageAndSquare = useCallback(() => {
-    if (canvasRef.current && imageRef.current) {
-      const canvas = canvasRef.current;
-      const ctx = canvas.getContext("2d");
-      const img = imageRef.current;
-
-      // Clear the canvas
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-      // Draw the image
-      ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
-
-      // Draw square if there is a last click position
-      if (lastClickPosition) {
-        const squareSize = sampleSize;
-
-        const x = lastClickPosition.x - squareSize / 2;
-        const y = lastClickPosition.y - squareSize / 2;
-
-        ctx.strokeStyle = "white";
-        ctx.lineWidth = 2;
-
-        ctx.strokeRect(x, y, squareSize, squareSize);
-
-        if (averageRGB) {
-          ctx.font = "10px Courier";
-          ctx.fillStyle = "white";
-          ctx.textAlign = "center";
-          ctx.fillText(
-            `R: ${averageRGB.r} G: ${averageRGB.g} B: ${averageRGB.b}`,
-            lastClickPosition.x,
-            y - 10
-          );
-        }
-      }
-    }
-  }, [lastClickPosition, canvasRef, imageRef, sampleSize, averageRGB]);
-
-  const handleSampleSizeChange = (newSize) => {
-    setSampleSize(newSize);
-    drawImageAndSquare();
-  };
-
-  useEffect(() => {
-    if (imageUploaded && canvasRef.current && imageRef.current) {
-      const canvas = canvasRef.current;
-      const img = imageRef.current;
-      const maxWidth = window.innerWidth * 0.8;
-      const maxHeight = window.innerHeight * 0.75;
-      let width = img.width;
-      let height = img.height;
-
-      if (width > maxWidth) {
-        height *= maxWidth / width;
-        width = maxWidth;
-      }
-
-      if (height > maxHeight) {
-        width *= maxHeight / height;
-        height = maxHeight;
-      }
-
-      canvas.width = width;
-      canvas.height = height;
-
-      setMaxSampleSize(Math.floor(width / 4));
-
-      drawImageAndSquare();
-    }
-  }, [imageUploaded, lastClickPosition, drawImageAndSquare]);
-
-  const handleCanvasClick = (event) => {
-    const canvas = canvasRef.current;
-    if (!canvas) {
-      console.error("Can't get canvas ref");
-      return;
-    }
-
-    const ctx = canvas.getContext("2d");
-    if (!ctx) {
-      console.error("Unable to get 2D context");
-      return;
-    }
-
-    const rect = canvas.getBoundingClientRect();
-    const x = Math.round(event.clientX - rect.left);
-    const y = Math.round(event.clientY - rect.top);
-
-    setLastClickPosition({ x, y });
-
-    const halfSampleSize = Math.floor(sampleSize / 2);
-    const imageData = ctx.getImageData(
-      Math.max(0, x - halfSampleSize),
-      Math.max(0, y - halfSampleSize),
-      Math.min(sampleSize, canvas.width - x + halfSampleSize),
-      Math.min(sampleSize, canvas.height - y + halfSampleSize)
-    );
-
-    // Calculate average color
-    let r = 0,
-      g = 0,
-      b = 0;
-
-    // Increment by 4 to skip the alpha channel
-    for (let i = 0; i < imageData.data.length; i += 4) {
-      r += imageData.data[i];
-      g += imageData.data[i + 1];
-      b += imageData.data[i + 2];
-    }
-    const pixelCount = imageData.data.length / 4; // Divide by 4 to get the number of pixels in the square
-
-    r = Math.round(r / pixelCount);
-    g = Math.round(g / pixelCount);
-    b = Math.round(b / pixelCount);
-
-    const rgb = `rgb(${r}, ${g}, ${b})`;
-    console.log(imageData);
-    console.log("Average RGB color:", rgb);
-
-    setSelectedColor(rgb);
-    setAverageRGB({ r, g, b });
-    const skinTone = getSkinTone(r, g, b);
-    setSkinTone(skinTone);
-  };
 
   return (
     <div className="flex flex-col items-center min-h-svh justify-center">
       <div className="w-full max-w-4xl p-4 md:p-8">
         {imageUploaded ? (
           <div className="flex flex-col items-center max-h-svh">
-            <Canvas
-              ref={canvasRef}
-              handleCanvasClick={handleCanvasClick}
-              className="rounded-xl mb-4"
-            />
+            <Canvas className="rounded-xl mb-4" />
             <div className="text-center bg-purple-50 p-4 rounded-lg shadow-md w-80 h-38 flex flex-col justify-center">
-              <ColourInfo
-                selectedColor={selectedColor}
-                skinTone={skinTone}
-                sampleSize={sampleSize}
-                onSampleSizeChange={handleSampleSizeChange}
-                maxSampleSize={maxSampleSize}
-              />
+              <ColourInfo />
             </div>
           </div>
         ) : (
           <UploadBox
-            handleImageUpload={handleImageUpload}
             ref={fileInputRef}
             title="Select an image to get started"
             prompt="Choose Image"
@@ -191,11 +30,6 @@ const SkinToneSelector = ({ setImageUploaded, imageUploaded }) => {
       </div>
     </div>
   );
-};
-
-SkinToneSelector.propTypes = {
-  setImageUploaded: PropTypes.func.isRequired,
-  imageUploaded: PropTypes.bool.isRequired,
 };
 
 export default SkinToneSelector;
--- src/components/UploadBox.jsx
+++ src/components/UploadBox.jsx
@@ -1,53 +1,53 @@
-import { forwardRef } from "react";
+import { forwardRef, useContext } from "react";
 import PropTypes from "prop-types";
+import { SkinToneContext } from "../store/SkinToneContext";
 
-const UploadBox = forwardRef(
-  ({ handleImageUpload, title, prompt, subtitle }, ref) => {
-    return (
-      <div className="text-center p-8 bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 rounded-lg shadow-lg">
-        <p className="text-white text-xl mb-6 font-semibold">{title}</p>
-        <label
-          htmlFor="fileInput"
-          className="group relative inline-flex items-center justify-center p-0.5 mb-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 cursor-pointer"
-        >
-          <span className="relative px-5 py-2.5 transition-all duration-500 ease-in-out bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0 hover:px-8">
-            <span className="flex items-center">
-              <svg
-                className="w-6 h-6 mr-2"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"
-                ></path>
-              </svg>
-              {prompt}
-            </span>
+const UploadBox = forwardRef(({ title, prompt, subtitle }, ref) => {
+  const { handleImageUpload } = useContext(SkinToneContext);
+
+  return (
+    <div className="text-center p-8 bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 rounded-lg shadow-lg">
+      <p className="text-white text-xl mb-6 font-semibold">{title}</p>
+      <label
+        htmlFor="fileInput"
+        className="group relative inline-flex items-center justify-center p-0.5 mb-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 cursor-pointer"
+      >
+        <span className="relative px-5 py-2.5 transition-all duration-500 ease-in-out bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0 hover:px-8">
+          <span className="flex items-center">
+            <svg
+              className="w-6 h-6 mr-2"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"
+              ></path>
+            </svg>
+            {prompt}
           </span>
-        </label>
-        <input
-          id="fileInput"
-          type="file"
-          ref={ref}
-          accept="image/*"
-          onChange={handleImageUpload}
-          className="hidden"
-        />
-        <p className="text-white text-sm mt-4">{subtitle}</p>
-      </div>
-    );
-  }
-);
+        </span>
+      </label>
+      <input
+        id="fileInput"
+        type="file"
+        ref={ref}
+        accept="image/*"
+        onChange={handleImageUpload}
+        className="hidden"
+      />
+      <p className="text-white text-sm mt-4">{subtitle}</p>
+    </div>
+  );
+});
 
 UploadBox.displayName = "UploadBox";
 
 UploadBox.propTypes = {
-  handleImageUpload: PropTypes.func.isRequired,
   title: PropTypes.string,
   prompt: PropTypes.string,
   subtitle: PropTypes.string,
