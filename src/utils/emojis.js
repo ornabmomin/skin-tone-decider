e0f1bcca93f9ba6e47b8d1edadda8aa6adeb3b2a
ornab <115734983+ornabmomin@users.noreply.github.com>
2024-07-29 22:02:35 -0400

    feat: :zap: Fetch emojis remotely

--- src/utils/emojis.js
+++ src/utils/emojis.js
@@ -1,7 +1,6 @@
-import data from "@emoji-mart/data";
+const EMOJI_DATA_URL = "https://cdn.jsdelivr.net/npm/@emoji-mart/data";
 
-// Emojis with skin tones that I don't want as options
-const additionalExclusions = [
+const ADDITIONAL_EXCLUSIONS = [
   "cop",
   "male-police-officer",
   "female-police-officer",
@@ -10,20 +9,63 @@ const additionalExclusions = [
   "vampire",
   "male_vampire",
   "female_vampire",
+  "man_and_woman_holding_hands",
+  "people_holding_hands",
+  "woman_and_man_holding_hands",
+  "two_women_holding_hands",
+  "two_men_holding_hands",
+  "couplekiss",
+  "woman-kiss-man",
+  "man-kiss-man",
+  "woman-kiss-woman",
+  "man-kiss-woman",
+  "couple_with_heart",
+  "woman-heart-man",
+  "man-heart-man",
+  "woman-heart-woman",
+  "man-heart-woman",
 ];
 
-const arrayOfEmojis = Object.values(data.emojis);
+let data = null;
+let skinToneEmojiList = [];
+let exclusionList = [];
 
-// List of emojis to randomly choose from for initial emoji
-export const skinToneEmojiList = [];
-
-// List of emojis to exclude from emoji picker
-export const exclusionList = [...additionalExclusions];
-
-for (const emoji of arrayOfEmojis) {
-  if (emoji.skins?.length > 1 && !additionalExclusions.includes(emoji.id)) {
-    skinToneEmojiList.push(emoji.id);
-  } else {
-    exclusionList.push(emoji.id);
+const fetchEmojiData = async () => {
+  try {
+    const response = await fetch(EMOJI_DATA_URL);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    return await response.json();
+  } catch (error) {
+    console.error("Error fetching emoji data:", error);
+    throw error;
   }
-}
+};
+
+const processEmojiData = () => {
+  const emojis = Object.values(data.emojis);
+
+  skinToneEmojiList = emojis
+    .filter(
+      (emoji) =>
+        emoji.skins?.length > 1 && !ADDITIONAL_EXCLUSIONS.includes(emoji.id)
+    )
+    .map((emoji) => emoji.id);
+
+  exclusionList = [
+    ...ADDITIONAL_EXCLUSIONS,
+    ...emojis
+      .filter((emoji) => !skinToneEmojiList.includes(emoji.id))
+      .map((emoji) => emoji.id),
+  ];
+};
+
+const initializeEmojiData = async () => {
+  data = await fetchEmojiData();
+  processEmojiData();
+};
+
+initializeEmojiData();
+
+export { data, skinToneEmojiList, exclusionList };
